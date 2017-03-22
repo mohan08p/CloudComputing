ce9cb49ab1a210668f891b178321e5179041ed4f
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-03-22 18:57:47 +0530

    fix: rails endpoint fix

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :railsapp do
+  scope '/railsapp' do
     resources :posts
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
